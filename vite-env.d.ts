cdb9dc136b19eb32099a7a83bac1727513807efa
Ing. Jorge Franco <110159185+ingenierojorgefranco@users.noreply.github.com>
2025-12-01 17:02:49 +0100

    feat: Add Dockerfile and vite env types
    Introduces a Dockerfile for containerizing the application and defines environment variable types for Vite. This enables easier deployment and development workflows.

---
+++ vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  // aquí puedes añadir más si las usas, por ejemplo:
+  // readonly VITE_OTRA_VARIABLE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
